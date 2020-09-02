a66a44a8263d9581d082799020711bb995ee08c8
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-09-02 20:30:02 -0300

    Select CSS
    CSS da página Select para tela de todos os tamanhos

--- pages/select.php
+++ pages/select.php
@@ -1,15 +1,29 @@
 <div class="select">
-    <?php
 
-        $array = $mysql->selectALL($pdo);
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Tipo de Animal</th>     
+            <th>Idade do Animal</th>
+        </tr>
 
-        foreach ($array as $animal) {
-            echo $animal['id'];
-            echo $animal['name'];
-            echo $animal['type'];
-            echo $animal['age'];
-            echo "<br>";
-        }
+        <?php
 
-    ?>
+            $array = $mysql->selectALL($pdo);
+
+            foreach ($array as $animal) {    
+                echo "<tr>";
+                echo "<td>".$animal['id']."</td>";
+                echo "<td>".$animal['name']."</td>";
+                echo "<td>".$animal['type']."</td>";
+                echo "<td>".$animal['age']."</td>";
+                echo "</tr>";
+            }
+
+        ?>
+
+    </table>
+
+    <a href="<?php echo INCLUDE_PATH ?>">Voltar para a página Inicial</a>
 </div>
