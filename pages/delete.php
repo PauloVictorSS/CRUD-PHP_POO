1931bf667ad610dbd9125fb52951ee0e348df4a5
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-09-06 20:20:56 -0300

    Deletar Animal
    Função para deletar um animal com base em seu id

--- pages/delete.php
+++ pages/delete.php
@@ -1,1 +1,39 @@
-Delete
+<?php
+    
+    if(isset($_POST["action"])){
+
+        $id = $_POST["idAnimal"];
+
+        $result = $mysql->deleteAnimal($pdo, $id);
+
+        if($result == 1)
+            echo "<div class='mensage green'><p>Animal deletado com sucesso!<p></div>";
+        else
+            echo "<div class='mensage red'><p>Algo deu errado<p></div>";
+    }
+    
+?>
+
+<div class="select">
+
+    <h1>Deletar Animal</h1>
+
+    <form action="<?php echo INCLUDE_PATH ?>delete" method="post">
+        <select name="idAnimal">
+            <?php
+            
+            $array = $mysql->selectALL($pdo);
+
+            foreach ($array as $animal) { 
+
+                echo "<option value='".$animal['id']."'>".$animal['name']."</option>";
+
+            }
+            
+            ?>
+        </select>
+        <button type="submit" name="action" value="action">Deletar Animal</button>
+    </form>
+
+    <a href="<?php echo INCLUDE_PATH ?>">Voltar para a página Inicial</a>
+</div>
